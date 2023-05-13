658d82994045f1e9b48013120c4c5765c6d74eb7
kotddari <kotddari@gmail.com>
2023-05-13 20:41:08 +0900

    Update AfterReturningAdvice.java

--- src/main/java/com/kim/board/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/kim/board/biz/common/AfterReturningAdvice.java
@@ -12,24 +12,24 @@ public class AfterReturningAdvice {
 	@AfterReturning(pointcut="PointcutCommon.aPointcut()", returning="obj")
 	public void printLogARA(JoinPoint jp, Object obj) {
 		
-//		// 클래스네임 가져오기
-//		String className = jp.getTarget().toString();
-//		className = className.substring(className.indexOf(".", 17) + 1, className.lastIndexOf("Impl"));
-//				
-//		// 메서드네임 가져오기
-//		String methodName = jp.getSignature().getName() + "()";
-//		String arg = jp.getArgs()[0].toString();
-//		arg = arg.substring(0, arg.indexOf(",")) + "]";
-//		
-//		System.out.println();
-//		System.out.println("[Service 로그]");
-//		System.out.println("● 수행 메서드: " + className + "." + methodName);
-//		System.out.println("● 수행 인자PK: " + arg);
-//
-//		if(obj != null) {
-//			System.out.println("● 수행 결과값: " + obj.getClass().getName());
-//		}
-//		System.out.println("――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+		// 클래스네임 가져오기
+		String className = jp.getTarget().toString();
+		className = className.substring(className.indexOf(".", 17) + 1, className.lastIndexOf("Impl"));
+				
+		// 메서드네임 가져오기
+		String methodName = jp.getSignature().getName() + "()";
+		String arg = jp.getArgs()[0].toString();
+		arg = arg.substring(0, arg.indexOf(",")) + "]";
+		
+		System.out.println();
+		System.out.println("[Service 로그]");
+		System.out.println("● 수행 메서드: " + className + "." + methodName);
+		System.out.println("● 수행 인자PK: " + arg);
+
+		if(obj != null) {
+			System.out.println("● 수행 결과값: " + obj.getClass().getName());
+		}
+		System.out.println("――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
 		
 	}
 	
