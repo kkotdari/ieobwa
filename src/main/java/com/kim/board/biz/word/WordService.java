c7705d081cb02ec6e84a294ec760b75f02dfdcfb
kotddari <kotddari@gmail.com>
2023-05-16 01:26:11 +0900

    Update WordService.java

--- src/main/java/com/kim/board/biz/word/WordService.java
+++ src/main/java/com/kim/board/biz/word/WordService.java
@@ -1,6 +1,11 @@
 package com.kim.board.biz.word;
 
+import java.util.List;
+
+import com.kim.board.biz.board.BoardVO;
+
 public interface WordService {
-	public boolean update(WordVO vo);
-	public WordVO selectOne(WordVO vo);
+	public boolean insert(WordVO vo);
+	public boolean delete(WordVO vo);
+	public List<BoardVO> selectAll(WordVO vo);
 }
