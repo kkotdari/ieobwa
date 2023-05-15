d486e52a6ed38f2343c079cecd918d957b46d06c
kotddari <kotddari@gmail.com>
2023-05-16 01:26:08 +0900

    Update WordDAO.java

--- src/main/java/com/kim/board/biz/word/WordDAO.java
+++ src/main/java/com/kim/board/biz/word/WordDAO.java
@@ -1,42 +1,36 @@
 package com.kim.board.biz.word;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
+
+import com.kim.board.biz.board.BoardVO;
 
 @Repository("wardDAO")
 public class WordDAO {
 	@Autowired
 	private SqlSessionTemplate myBatis;
 
-	public boolean update(WordVO vo) {
-		WordVO wvo = selectOne(vo); 
-		if(wvo != null) {
-			int res2 = myBatis.update("WordDAO.updateWord", vo);
-			if (res2 < 1) {
+	public boolean insert(WordVO vo) {
+		int res = myBatis.insert("WordDAO.insertWord", vo);
+		if (res < 1) {
 				return false;
-			}
-			return true;
 		}
-		else {
-			int res2 = myBatis.insert("WordDAO.insertWord", vo);
-			if (res2 < 1) {
-				return false;
-			}
-			return true;
-		}
+		return true;
 	}
 	
-	public WordVO selectOne(WordVO vo) {
-		try {
-			return myBatis.selectOne("WordDAO.selectOneWord", vo);
-		} catch (EmptyResultDataAccessException e) {
-			System.out.println("WordDAO selectOne 결과 없음");
-		} catch (Exception e) {
-			e.printStackTrace();
+	public boolean delete(WordVO vo) {
+		int res = myBatis.delete("WordDAO.deleteWord", vo);
+		if (res < 1) {
+				return false;
 		}
-		return null;
+		return true;
+	}
+	
+	public List<BoardVO> selectAll(WordVO vo) {
+		return myBatis.selectList("WordDAO.selectAllWord", vo);
 	}
 }
 
