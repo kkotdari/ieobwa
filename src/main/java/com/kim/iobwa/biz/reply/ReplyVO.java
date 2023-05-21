2011dc3feac59c7291def96fb1e26669bfd72738
kotddari <kotddari@gmail.com>
2023-05-21 21:41:52 +0900

    Update ReplyVO.java

--- src/main/java/com/kim/iobwa/biz/reply/ReplyVO.java
+++ src/main/java/com/kim/iobwa/biz/reply/ReplyVO.java
@@ -7,6 +7,8 @@ public class ReplyVO {
 	private int parentNum; // 대댓글의 경우 부모 댓글 번호
 	private int replyStep; // 댓글 단계
 	private String replyDate; // 댓글 작성일
+	private String replyWriter; // 댓글 작성자
+	private String replyPassword; // 댓글 암호
 	private String replyContent; // 댓글 내용
 	// 멤버변수(VO에만 존재)
 	// getter, setter
@@ -40,6 +42,18 @@ public class ReplyVO {
 	public void setReplyDate(String replyDate) {
 		this.replyDate = replyDate;
 	}
+	public String getReplyWriter() {
+		return replyWriter;
+	}
+	public void setReplyWriter(String replyWriter) {
+		this.replyWriter = replyWriter;
+	}
+	public String getReplyPassword() {
+		return replyPassword;
+	}
+	public void setReplyPassword(String replyPassword) {
+		this.replyPassword = replyPassword;
+	}
 	public String getReplyContent() {
 		return replyContent;
 	}
@@ -50,6 +64,7 @@ public class ReplyVO {
 	@Override
 	public String toString() {
 		return "ReplyVO [replyNum=" + replyNum + ", boardNum=" + boardNum + ", parentNum=" + parentNum + ", replyStep="
-				+ replyStep + ", replyDate=" + replyDate + ", replyContent=" + replyContent + "]";
+				+ replyStep + ", replyDate=" + replyDate + ", replyWriter=" + replyWriter + ", replyPassword="
+				+ replyPassword + ", replyContent=" + replyContent + "]";
 	}
 }
