cec4442fd3e090dd3a1b81b656c57bbfe288f620
kotddari <kotddari@gmail.com>
2023-05-21 17:50:02 +0900

    Update ReplyService.java

--- src/main/java/com/kim/iobwa/biz/reply/ReplyService.java
+++ src/main/java/com/kim/iobwa/biz/reply/ReplyService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public interface ReplyService {
 	public boolean insert(ReplyVO vo);
+	public boolean update(ReplyVO vo);
 	public boolean delete(ReplyVO vo);
 	public List<ReplyVO> selectAll(ReplyVO vo);
+	public ReplyVO selectOne(ReplyVO vo);
 }
