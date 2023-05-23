52d69d8e1cb28a5595257fc88bb322938e3093ea
kotddari <kotddari@gmail.com>
2023-05-23 20:34:42 +0900

    Update Crawling.java

--- src/main/java/com/kotddari/ieobwa/biz/common/Crawling.java
+++ src/main/java/com/kotddari/ieobwa/biz/common/Crawling.java
@@ -16,7 +16,7 @@ import com.kotddari.ieobwa.biz.board.BoardVO;
 
 @Component
 public class Crawling {
-	final String SWITCH = "Y"; // ※ 크롤링 여부 (Y/N)
+	final String SWITCH = "N"; // ※ 크롤링 여부 (Y/N)
 	final String WEB_DRIVER_ID = "webdriver.chrome.driver"; // 드라이버 ID
 	final String WEB_DRIVER_PATH = "F:/Dev/kotddari/workspace/ieobwa/src/main/webapp/Source/chromedriver.exe"; // 드라이버
 	final int BOARD = 100; // ※ 크롤링할 게시물 개수 (범위는 5~100이며 클수록 시간이 늘어나므로 30이하로 하는 것을 추천!)
